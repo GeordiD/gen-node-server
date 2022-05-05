717e223e4c390e31caf6b0cffb2b85692ecc6396
GeordiD <geordi.dosher@gmail.com>
2022-05-04 22:07:25 -0500

    Adding mongo connection

---
+++ server/common/db-client.ts
@@ -0,0 +1,15 @@
+import { MongoClient } from 'mongodb';
+
+const uri = process.env['MONGODB_URI'];
+
+if (!uri) {
+  throw new Error('Please add your Mongo URI to .env.local');
+}
+
+const client = new MongoClient(uri);
+const dbClientConnection = client.connect();
+
+// Export a module-scoped MongoClient promise.
+// By doing this in a separate module,
+// the client can be shared across functions.
+export default dbClientConnection;
--- server/services/examples.service.ts
+++ server/services/examples.service.ts
@@ -1,3 +1,4 @@
+import dbClientConnection from '@/common/db-client';
 import L from '@/common/logger';
 
 let id = 0;
@@ -12,9 +13,18 @@ const examples: Example[] = [
 ];
 
 export class ExamplesService {
-  all(): Promise<Example[]> {
-    L.info(examples, 'fetch all examples');
-    return Promise.resolve(examples);
+  async all(): Promise<Example[]> {
+    const client = await dbClientConnection;
+    const cursor = await client
+      .db()
+      .collection('examples')
+      .find<Example>({})
+      .limit(10);
+    const result = await cursor.toArray();
+
+    L.info(result, 'fetch all examples');
+
+    return Promise.resolve(result);
   }
 
   byId(id: number): Promise<Example> {
