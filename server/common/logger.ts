c6302a10c688be506541c85b75c95508638a274d
GeordiD <geordi.dosher@gmail.com>
2022-05-04 20:09:50 -0500

    Setup for logging to a file, but commented it out until I learn more

--- server/common/logger.ts
+++ server/common/logger.ts
@@ -1,8 +1,16 @@
 import pino from 'pino';
 
-const l = pino({
-  name: process.env.APP_ID,
-  level: process.env.LOG_LEVEL,
-});
+const streams = [
+  { stream: process.stdout },
+  // { stream: pino.destination('logs/logger.log') },
+];
+
+const l = pino(
+  {
+    name: process.env.APP_ID,
+    level: process.env.LOG_LEVEL,
+  },
+  pino.multistream(streams)
+);
 
 export default l;
